7cb169d4f43ac897df4559d62820da56de7d3f80
Atul singh bhandari <atulbhan40@gmail.com>
2022-11-09 11:24:32 +0530

    first commit

---
+++ ValidationTest/ValidationTest/DAL/IUserDALDetails.cs
@@ -0,0 +1,11 @@
+﻿using ValidationTest.Models;
+
+namespace ValidationTest.DAL
+{
+    public interface IUserDALDetails
+    {
+        List<UserDetails> GetUserList();
+
+        List<CountryDetails> GetCountryDetails();
+    }
+}
