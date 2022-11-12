9f26ccdd90b56a9d8233e4368247de8b66325dda
Atul singh bhandari <atulbhan40@gmail.com>
2022-11-12 22:02:55 +0530

    commit

--- ValidationTest/ValidationTest/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ValidationTest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
